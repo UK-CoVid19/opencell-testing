2329561fc59c7decfe2a6bd6464db2b010081626
3DPrintScanner <1988anthonythomas@gmail.com>
2020-09-01 22:47:45 +0100

    Clean up views

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :plate
   belongs_to :user
-<<<<<<< HEAD
   has_one_attached :result_file
-=======
-  has_many_attached :result_files
-  validates :result_files, length: { maximum: 2, minimum: 2 }
->>>>>>> WIP split client model and user model
   has_many :test_results, dependent: :destroy
   accepts_nested_attributes_for :test_results
 end
