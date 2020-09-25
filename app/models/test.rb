6613a71c9723ba9598524b35da11c779bb50bc09
3DPrintScanner <1988anthonythomas@gmail.com>
2020-09-25 17:58:00 +0100

    update accepted file types

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :plate
   belongs_to :user
   has_one_attached :result_file
-  validates :result_file, presence: true, blob: { content_type: ['text/csv'] }
+  validates :result_file, presence: true, blob: { content_type: ['text/csv', 'application/vnd.ms-excel'] }
   validates_with AntivirusValidator, attribute_name: :result_file
   has_many :test_results, dependent: :destroy
   accepts_nested_attributes_for :test_results
