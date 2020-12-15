2fe2236ea75ee6030aa03c263e6bc4c7912ed5cc
3DPrintScanner <1988anthonythomas@gmail.com>
2020-12-15 15:31:03 +0000

    bleh

--- db/migrate/20201211145031_add_user_to_plate.rb
+++ db/migrate/20201211145031_add_user_to_plate.rb
@@ -1,16 +1,17 @@
 class AddUserToPlate < ActiveRecord::Migration[6.0]
   def up
     add_reference :plates, :user, null: true, foreign_key: false
-    Plate.all.each do |p|
-      user = p.samples.first.records.where(state: Sample.states[:preparing]).first.user
-      next if user.nil?
-      p.update(user: user)
-      p.save!
-    end
     pwd = Devise.friendly_token.first(12)
     dummy_user = !User.all.any? ? User.create(email: 'dummy@user.com', password: pwd, password_confirmation: pwd) : User.first
-    Plate.where(user_id: nil).update_all(user_id: dummy_user.id)
-
+    Plate.all.each do |p|
+      user = p.samples.first.records.where(state: Sample.states[:preparing]).first.user
+      if user.nil?
+        p.update(user: dummy_user)
+      else
+        p.update(user: user)
+      end
+      p.save!
+    end
     sleep(5.seconds)
     change_column_null :plates, :user_id, false
     add_foreign_key :plates, :users
