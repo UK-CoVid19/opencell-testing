5262e59314e2999a1caf8a424c159bf0f4ed7441
3DPrintScanner <1988anthonythomas@gmail.com>
2020-12-17 16:02:14 +0000

    remove if no data

--- db/migrate/20201211145031_add_user_to_plate.rb
+++ db/migrate/20201211145031_add_user_to_plate.rb
@@ -1,19 +1,23 @@
 class AddUserToPlate < ActiveRecord::Migration[6.0]
   def up
     add_reference :plates, :user, null: true, foreign_key: false
-    pwd = Devise.friendly_token.first(12)
-    question = SecurityQuestion.find_or_create_by(name: 'Where were you born?', locale: :en)
-    dummy_user = !User.all.any? ? User.create!(email: 'dummy@example.com', password: 'b]^$t&JF}?kN3Z5n', password_confirmation: 'b]^$t&JF}?kN3Z5n', role: User.roles[:staff], security_question: question, security_question_answer:"London") : User.first
-    Plate.all.each do |p|
-      user = p.samples.first.records.where(state: Sample.states[:preparing]).first.user
-      if user.nil?
-        p.update(user: dummy_user)
-      else
-        p.update(user: user)
+    
+    if Plate.all.any?
+    
+      pwd = Devise.friendly_token.first(12)
+      question = SecurityQuestion.find_or_create_by(name: 'Where were you born?', locale: :en)
+      dummy_user = !User.all.any? ? User.create!(email: 'dummy@example.com', password: 'b]^$t&JF}?kN3Z5n', password_confirmation: 'b]^$t&JF}?kN3Z5n', role: User.roles[:staff], security_question: question, security_question_answer:"London") : User.first
+      Plate.all.each do |p|
+        user = p.samples.first.records.where(state: Sample.states[:preparing]).first.user
+        if user.nil?
+          p.update(user: dummy_user)
+        else
+          p.update(user: user)
+        end
+        p.save!
       end
-      p.save!
+      sleep(5.seconds)
     end
-    sleep(5.seconds)
     change_column_null :plates, :user_id, false
     add_foreign_key :plates, :users
 
