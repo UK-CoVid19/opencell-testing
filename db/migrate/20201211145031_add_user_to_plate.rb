c793479833271326d5453155859c4c7978e85cd4
3DPrintScanner <1988anthonythomas@gmail.com>
2020-12-17 16:58:14 +0000

    blah

--- db/migrate/20201211145031_add_user_to_plate.rb
+++ db/migrate/20201211145031_add_user_to_plate.rb
@@ -2,11 +2,12 @@ class AddUserToPlate < ActiveRecord::Migration[6.0]
   def up
     add_reference :plates, :user, null: true, foreign_key: false
     
-    if !Plate.all.empty?
+    plates = Plate.all
+    if !plates.empty?
       pwd = Devise.friendly_token.first(12)
       question = SecurityQuestion.find_or_create_by(name: 'Where were you born?', locale: :en)
       dummy_user = !User.all.any? ? User.create!(email: 'dummy@example.com', password: 'b]^$t&JF}?kN3Z5n', password_confirmation: 'b]^$t&JF}?kN3Z5n', role: User.roles[:staff], security_question: question, security_question_answer:"London") : User.first
-      Plate.all.each do |p|
+      plates.each do |p|
         user = p.samples.first.records.where(state: Sample.states[:preparing]).first.user
         if user.nil?
           p.update(user: dummy_user)
