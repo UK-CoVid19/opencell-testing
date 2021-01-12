0aee0e60e2ddde07d001025393eabafd43b82ab7
3DPrintScanner <1988anthonythomas@gmail.com>
2021-01-12 14:37:13 +0000

    update datatables to show plate id

--- app/datatables/test_datatable.rb
+++ app/datatables/test_datatable.rb
@@ -4,7 +4,7 @@ class TestDatatable < AjaxDatatablesRails::ActiveRecord
 
   extend Forwardable
 
-  def_delegators :@view, :get_badge, :button_tag, :policy_scope, :link_to, :rails_blob_path
+  def_delegators :@view, :get_badge, :button_tag, :policy_scope, :link_to, :rails_blob_path, :plate_path
 
   def initialize(params, opts = {})
     @view = opts[:view_context]
@@ -26,6 +26,7 @@ class TestDatatable < AjaxDatatablesRails::ActiveRecord
       {
         # example:
         id: record.id,
+        plate: link_to( record.plate&.id, plate_path(record.plate&.id)),
         created_at: record.created_at.strftime('%a %d %b %H:%M'),
         tested_by: record.user.email,
         result_file: link_to(rails_blob_path(record.result_file, disposition: "attachment")) { button_tag("Results", class: 'btn btn-info') },
