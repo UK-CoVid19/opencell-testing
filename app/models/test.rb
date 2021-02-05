7918f6c5269a5b8fbfa9114de7f80fdeec48181d
3DPrintScanner <1988anthonythomas@gmail.com>
2021-02-05 14:50:47 +0000

    update to allow zip

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   belongs_to :user
   has_one_attached :result_file
   validates_uniqueness_of :plate_id
-  validates :result_file, presence: true, blob: { content_type: ['text/csv', 'application/vnd.ms-excel'] }
+  validates :result_file, presence: true, blob: { content_type: ['text/csv', 'application/vnd.ms-excel', 'application/zip'] }
   validates_with AntivirusValidator, attribute_name: :result_file
   has_many :test_results, dependent: :destroy
   accepts_nested_attributes_for :test_results
